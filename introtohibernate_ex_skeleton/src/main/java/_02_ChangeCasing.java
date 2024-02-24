af0f620342b3a8a2c2f28f0e33811bd97d5844a4
Milenkov <krismessi1995@gmail.com>
2024-02-24 14:05:52 +0100

    Changing Casting
    Persisting all towns from the database. Detaching those whose name length is more than 5 symbols. Then transforming the names of all attached towns to uppercase and saving them to the database.

---
+++ introtohibernate_ex_skeleton/src/main/java/_02_ChangeCasing.java
@@ -0,0 +1,30 @@
+import entities.Town;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import java.util.List;
+
+public class _02_ChangeCasing {
+    public static void main(String[] args) {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("PU_Name");
+        EntityManager em = factory.createEntityManager();
+        em.getTransaction().begin();
+
+        Query from_Town = em.createQuery("SELECT t FROM Town t", Town.class);
+        List<Town> resultList = from_Town.getResultList();
+
+        for (Town town : resultList) {
+            String name = town.getName();
+            if (name.length() <= 5) {
+                String toUpper = name.toUpperCase();
+                town.setName(toUpper);
+
+                em.persist(town);
+            }
+        }
+
+        em.getTransaction().commit();
+    }
+}
